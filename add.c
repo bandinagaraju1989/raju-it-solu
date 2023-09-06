6c65c365ed129b3fc036aedaa047e257322d202b
Marri.Mallika <marri.mallika@gmail.com>
2023-09-06 00:57:57 +0000

    Modified file add.cfile

--- add.c
+++ add.c
@@ -1,7 +1,14 @@
-# include<stdio>
-int main()
-{
- int a=10,b=20,c;
- c=a+b;
- print("sum of two numbers are:",c);
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
 }
